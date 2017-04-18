6d40dd010852f4422fdc5b88a6e3f4362e6fa720
Gustavo Sobral <ghsobral@gmail.com>
2017-04-18 11:54:07 +0200

    Extract the Full Scan similarity calculation logic
    Create the ScanAlgorithmSimilarity class

--- lib/space_invaders/core/scan_algorithm.rb
+++ lib/space_invaders/core/scan_algorithm.rb
@@ -16,10 +16,11 @@ module SpaceInvaders
       #   area indentified on them
       def execute
         border_height, border_width = border_calculations
-        bordered_image = apply_border(border_height, border_width, 'x')
+        new_image = new_bordered_image(border_height, border_width, 'x')
+        bordered_image = apply_border(border_height, border_width, new_image)
 
         similarity_matrices = space_invaders.map do |space_invader|
-          calculate_similarity(space_invader, bordered_image)
+          ScanAlgorithmSimilarity.new(space_invader, 'x', bordered_image).calculate
         end
 
         result = merge_similarities(similarity_matrices, border_height, border_width)
@@ -36,79 +37,24 @@ module SpaceInvaders
         return border_height, border_width
       end
 
+      # Create a new full bordered image
+      def new_bordered_image(border_height, border_width, border)
+        Array.new(radar_image.height + (border_height * 2)) do
+          Array.new(radar_image.width + (border_width * 2), border)
+        end
+      end
+
       # Apply border on the radar image
       #
       # @return [RadarImage]
       #   The new radar image bordered
-      def apply_border(border_height, border_width, border)
-        bordered_image = Array.new(radar_image.height + (border_height * 2)) do
-          Array.new(radar_image.width + (border_width * 2), border)
-        end
-
+      def apply_border(border_height, border_width, bordered_image)
         radar_image.content[0..radar_image.height].each_with_index do |row, row_index|
           width_range = border_width..(border_width + radar_image.width - 1)
           bordered_image[border_height + row_index][width_range] = row
         end
 
         RadarImage.new(radar_image.id, bordered_image)
-      end
-
-      # Calculate the similarity matrix between the
-      # bordered radar image and the space invader
-      def calculate_similarity(invader, bordered_image)
-        similarity_matrix = Array.new(bordered_image.height) { Array.new(bordered_image.width, 0) }
-
-        max_row_index = (bordered_image.height - invader.height)
-        max_column_index = (bordered_image.width - invader.width)
-
-        bordered_image.content[0..max_row_index].each_with_index do |row, row_index|
-          row[0..max_column_index].each_with_index do |_, index|
-            row_range, column_range = calculate_window_range(row_index, index, invader)
-            radar_window = bordered_image.content_range(row_range, column_range)
-            similarity = window_similarity(invader, radar_window)
-            update_similarity(similarity_matrix, similarity, row_range, column_range) if similarity
-          end
-        end
-
-        similarity_matrix
-      end
-
-      # Calculate the range of radar image window under comparison
-      def calculate_window_range(row_index, index, invader)
-        row_range = (row_index..(invader.height + row_index - 1))
-        column_range = (index..(invader.width + index - 1))
-
-        return row_range, column_range
-      end
-
-      def window_similarity(invader, radar_window)
-        return unless radar_window.flatten.count('x') < (radar_window.flatten.size / 2.0)
-        window_comparison(radar_window, invader)
-      end
-
-      # Calculate the percentage of similiratiry between the
-      # space invader and the radar window (Ignore border indexes)
-      #
-      # @return [Float]
-      #   The percentage similarity
-      def window_comparison(radar_window, invader)
-        equal_pixels = radar_window.zip(invader.image).map do |x, y|
-          x.zip(y).map do |w, z|
-            w == z if w != 'x'
-          end
-        end
-
-        equal_pixels = equal_pixels.flatten.compact
-        equal_pixels.count(true) / equal_pixels.size.to_f
-      end
-
-      # Update the similarity matrix with the new similarity
-      def update_similarity(similarity_matrix, similarity, row_range, column_range)
-        row_range.to_a.each do |i|
-          column_range.to_a.each do |j|
-            similarity_matrix[i][j] = similarity if similarity_matrix[i][j] < similarity
-          end
-        end
       end
 
       # Merge the similarity between the similarity matrices
@@ -129,6 +75,7 @@ module SpaceInvaders
         output
       end
 
+      # Subset the similarity matrix ignoring the borders values
       def similarity_subset(similarity_matrix, border_height, border_width)
         max_range = (radar_image.height + border_height - 1)
         similarity_matrix[border_height..max_range].map do |row|
